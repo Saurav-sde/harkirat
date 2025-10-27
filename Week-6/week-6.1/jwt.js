0ac7e208fa51857643151263a15a0936889e82aa
Saurav Kumar <saurav.sde.cs@gmail.com>
2025-10-28 00:08:55 +0530

    authorization, jsonwebtokens

---
+++ Week-6/week-6.1/jwt.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "sadhisjidsjiooigogtrji";
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+// returns a random long string
+function generateToken(){
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post('/signup',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // we can do checks here  
+    if(users.find(u => u.username === username)) {
+        res.json({
+            message: "you are already registerd . please sign in to continue"
+        })
+        return;
+    }
+
+    if(username.length < 5) {
+        res.json({
+            message: "username length is too small"
+        })
+        return;
+    }
+
+    if(password.length < 5) {
+        res.json({
+            message: "password length is too small"
+        })
+        return;
+    }
+
+    // later we will learn about input validation using zod
+    users.push({
+        username: username,
+        password: password,
+    })
+
+    res.json({
+        msg: "you are signed up" 
+    })
+})
+
+app.post('/signin',(req,res)=>{
+    const {username, password} = req.body;
+    const foundUser = users.find(u => u.username === username && u.password === password);
+    if(foundUser) {
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET)
+
+        res.status(202).json({
+            token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Invalid username or password"
+        })
+    }
+})
+
+app.get("/me", (req,res) => {
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    const username = decodedInfo.username;
+
+    const foundUser = users.find(u => u.username === username);
+    if(foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message: "token invalid"
+        })
+    }
+})
+
+app.listen(3000);
---
+++ Week-6/week-6.2/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+app.use(express.json());
+const JWT_SECRET = "nkskfskkeksjdj";
+
+const users = [];
+
+function auth(req,res,next) {
+    const token = req.headers.token;
+    const decodedInfo = jwt.verify(token,JWT_SECRET);
+    const username = decodedInfo.username;
+    if(!username)
+        return res.json({msg:"You are not logged in"});
+
+    req.username = username;
+    next();
+}
+
+function logger(req,res,next){
+    console.log(req.method + " request came");
+    next();
+}
+
+app.use(logger);
+
+app.post('/signup',(req,res)=>{
+    const {username,password} = req.body;
+    if(!username || !password)
+        return res.json({msg:"username or password required"});
+
+    users.push({username,password});
+    res.json({msg:"You are signed up"});
+    console.log(users);
+})
+
+app.post('/signin',(req,res)=>{
+    const {username,password} = req.body;
+    
+    const foundUser = users.find(u => (u.username === username && u.password === password));
+    if(foundUser) {
+        const token = jwt.sign({username},JWT_SECRET);
+        return res.json({token});
+    } else {
+        return res.json({msg:"Invalid username or password"});
+    }
+})
+
+app.get("/me", auth, (req,res) => {
+    const foundUser = users.find(u => u.username === req.username);
+    if(!foundUser)
+        return res.json({msg:"You are not logged in"})
+    return res.json(foundUser);
+})
+
+app.listen(3000);
